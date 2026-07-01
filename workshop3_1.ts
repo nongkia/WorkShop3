c58630ed184c6ad048198457e242424c2672bff0
patiphon <nongkia8531@gmail.com>
2026-07-01 18:53:13 +0700

    ส่งงาน workshop3_1

---
+++ workshop3_1.ts
@@ -0,0 +1,10 @@
+//function CalculateArea(width: number,height: number):number{
+//    return width * height;
+//}
+
+const CalculateArea = (width:number, height: number) =>{
+    return width * height;
+}
+let area:number = CalculateArea(6,6);
+
+console.log(area);
